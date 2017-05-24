d4be0febec41c23c711b04dd5ee880bb97fadb69
Edi.Y <edi.yulianto@divusi.com>
2017-05-24 10:11:55 +0700

    Fix bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
+// require_once __DIR__.'/../vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php'; 
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
